d4dd44d3c176b055ced118c4440aa34d388b2bde
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2022-05-23 15:17:41 -0300

    first delivery commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate: uuidValidate } = require('uuid');
+const { json } = require('express/lib/response');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +11,86 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username );
+
+  if (!user) {
+    return response
+    .status(404)
+    .json({error: 'User not found!'})
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro == true) {
+    return next();
+  }
+
+  if ((user.pro == false) && (user.todos.length < 10)) {
+    return next();
+  }
+
+  return response
+  .status(403)
+  .json({error: 'Mensagem do erro'})
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username );
+
+  if (!user) {
+    return response
+    .status(404)
+    .json({error: 'Mensagem do erro'})
+  }
+
+  if(!uuidValidate(user.id)) {
+    return response
+    .status(404)
+    .json({error: 'Mensagem do erro'})
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id );
+
+  if (!todo) {
+    return response
+    .status(404)
+    .json({error: 'Mensagem do erro'})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id );
+
+  if (!user) {
+    return response
+    .status(404)
+    .json({error: 'Mensagem do erro'})
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
@@ -38,7 +106,7 @@ app.post('/users', (request, response) => {
     id: uuidv4(),
     name,
     username,
-    pro: false,
+    pro: false, 
     todos: []
   };
 
